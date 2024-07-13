e625a52484bb3f79034b3e8d7cdc8b104be11f24
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-07-13 18:30:02 -0300

    model

--- app/controllers/rewards_given_controller.rb
+++ app/controllers/rewards_given_controller.rb
@@ -1,13 +1,22 @@
 class RewardsGivenController < ApplicationController
   def new
+    @rewards_given = RewardGiven.new
   end
 
   def create
+    @rewards_given = RewardGiven.new(rewards_given_params)
   end
 
   def index
   end
 
   def destroy
+  end
+
+  private
+
+  def rewards_given_params
+    params.require(:rewards_given).permit(:reward_id, :user_id)
+
   end
 end
