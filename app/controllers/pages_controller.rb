4d0926a82923f968b8c50fbef4adde440e134524
felipe imbimbo <f_imbimbo@hotmail.com>
2024-07-14 11:25:48 -0300

    feat: home page charts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @total_sales = Challenge.joins(:challenge_dones).sum(:price)
+
+  @challenges_done_prices = Challenge.joins(:challenge_dones)
+                                    .where('challenges.price > 0')
+                                     .group(:id, :title)
+                                     .sum(:price)
+  @labels_sales = @challenges_done_prices.keys.map { |id, title| title }
+  @data_sales = @challenges_done_prices.values
     @challenges_done_count = ChallengeDone.group(:challenge_id).count
 
     @labels_challenges = @challenges_done_count.keys.map { |id| Challenge.find(id).title }
