663ffdeecf89ac8616792ae01c458a1ae7ce759a
Fred Franca <fredfranssa@gmail.com>
2024-07-14 09:44:29 -0300

    seed-combos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   has_many :challenge_dones
-=======
 
   has_many :reward_given
->>>>>>> main
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
